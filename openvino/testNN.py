cf2eb333399210a1904996f543fa99dc88a9ff75
BinhDS <binh.duongsy@gmail.com>
2021-06-05 19:56:21 +0100

    run model conver from tf to blob in video depthai ok

--- openvino/testNN.py
+++ openvino/testNN.py
@@ -7,7 +7,6 @@ import depthai as dai
 import numpy as np
 import BEN_detectFinger as finger  
 import time  
-import torchvision.transforms as transforms
 
 ben = finger.handLandmarks()  
 pTime = 0  
@@ -15,9 +14,7 @@ cTime = 0
 
 labels  = ['Ok', 'Silent', 'Dislike', 'Like', 'Hi', 'Hello', 'Stop' , ' ' ]
 
-
-#blobPath = '/home/pcwork/ai/ftech/finger/CNN/modelPytorch/openvino/model2.blob' 
-blobPath = '/home/pcwork/ai/ftech/finger/CNN/modelTensorflow/modelOpenvino/modelHand.blob'  
+blobPath ='./../modelTensorflow/modelOpenvino/modelHand.blob'  
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-nd', '--no-debug', action="store_true", help="Prevent debug output")
@@ -41,28 +38,48 @@ detection_nn.setBlobPath(str(Path(blobPath).resolve().absolute()))
 if camera:
     print("Creating Color Camera...") 
 
+    #Define a source - color camera 
+    cam_rbg = pipeline.createColorCamera()
+    cam_rbg.setVideoSize(640, 480 ) 
+    cam_rbg.setBoardSocket( dai.CameraBoardSocket.RGB ) 
+    cam_rbg.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P) 
+    cam_rbg.setInterleaved(True)  
+    cam_rbg.setFps(40)  
 
-    cam_rgb = pipeline.createColorCamera()
-    cam_rgb.setPreviewSize(480, 480)
-    cam_rgb.setInterleaved(False)
-    cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    cam_rgb.setBoardSocket(dai.CameraBoardSocket.RGB)
-    cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+    #create output 
 
-    cam_xout = pipeline.createXLinkOut()
-    cam_xout.setStreamName("rgb")
-    cam_rgb.preview.link(cam_xout.input)
+    xout_video = pipeline.createXLinkOut() 
+    xout_video.setStreamName("video") 
+    
+    cam_rbg.video.link(xout_video.input) 
 
-    print("Creating ImageManip node...")
-    manip = pipeline.createImageManip()
-    manip.initialConfig.setResize(26, 26)
-    cam_rgb.preview.link(manip.inputImage)
-    manip.out.link(detection_nn.input)
 
-else:
-    face_in = pipeline.createXLinkIn()
-    face_in.setStreamName("in_nn")
-    face_in.out.link(detection_nn.input)
+    #cam_rbg.video.link(detection_nn.input)
+  
+
+
+
+    #cam_rgb = pipeline.createColorCamera()
+    #cam_rgb.setPreviewSize(480, 480)
+    #cam_rgb.setInterleaved(False)
+    #cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    #cam_rgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+    #cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+    #cam_xout = pipeline.createXLinkOut()
+    #cam_xout.setStreamName("rgb")
+    #cam_rgb.preview.link(cam_xout.input)
+
+    #print("Creating ImageManip node...")
+    #manip = pipeline.createImageManip()
+    #manip.initialConfig.setResize(26, 26)
+    #cam_rgb.preview.link(manip.inputImage)
+    #manip.out.link(detection_nn.input)
+
+#create viture image input for neural network
+imgNN = pipeline.createXLinkIn()
+imgNN.setStreamName("in_nn")
+imgNN.out.link(detection_nn.input)
 
 # Create outputs
 xout_nn = pipeline.createXLinkOut()
@@ -91,8 +108,6 @@ def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     ben =  e_x / e_x.sum(axis=0)   
-    print ( ben  ) 
-
     return e_x / e_x.sum(axis=0)
 
 
@@ -107,56 +122,66 @@ with dai.Device(pipeline) as device:
 
     # Output queues will be used to get the rgb frames and nn data from the outputs defined above
     if camera:
-        q_rgb = device.getOutputQueue(name="rgb", maxSize=1, blocking=False)
-        #detection_in = device.getInputQueue("in_nn") 
+        q_rgb = device.getOutputQueue(name="video", maxSize=4, blocking=False)
     else:
         cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
-        detection_in = device.getInputQueue("in_nn")
+    detection_in = device.getInputQueue("in_nn")
     q_nn = device.getOutputQueue(name="nn", maxSize=1, blocking=False)
 
 
     def should_run():
         return cap.isOpened() if args.video else True
 
-
     def get_frame():
         if camera:
             in_rgb = q_rgb.get()
-            new_frame = np.array(in_rgb.getData()).reshape((3, in_rgb.getHeight(), in_rgb.getWidth())).transpose(1, 2, 0).astype(np.uint8)
-            new_frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2RGB)
-            return True, np.ascontiguousarray(new_frame)
+            img = in_rgb.getCvFrame() 
+            #new_frame = cv2.cvtColor(new_frame, cv2.COLOR_BGR2RGB)
+            return True, img 
         else:
             return cap.read()
 
     result = None
+    counter = 0 
+    label = ''
+    conf = 0 
 
-    while should_run():
+    while should_run(): 
+        # Read image from video or camera 
         read_correctly, img  = get_frame()
 
+        # Break if return false 
         if not read_correctly:
             break
         
+        # processing to find point for each finger and, bouding box 
         ben.showFinger( img )
         pointList, box  = ben.storePoint ( img )
         check , img1 , img2 = ben.drawAndResize( img , box )
-            
+        
+        #Drawing bouding box for 1 hand ( hand is higher ) 
+
         if len ( box ) != 0 :
 
-            cv2.rectangle( img , ( box[0] - 20 , box[1] - 20  ) , ( box[2] + 20 , box[3]+ 20  ) , (0,255,0),2)
+            cv2.rectangle( img , ( box[0] - 20 , box[1] - 20  ) , ( box[2] + 20 , box[3]+ 20  ) , (0,255,0),2) 
+        
+        # define curent time and previous time to calc fps 
         cTime = time.time()
-        fps = 1/( cTime - pTime )
-        pTime = cTime
-        cv2.putText( img , f"Fps: {int(fps)}" , (6,50) , cv2.FONT_HERSHEY_PLAIN,2, (255, 0, 255 ) ,3 )
+        counter +=1 
+        if ( cTime - pTime ) > 1 :
+            fps = counter/( cTime - pTime ) 
+            counter = 0 
+            pTime = cTime
 
 
-
+        #Check bouding box have hand or not 
         if check and img2.size == 100*100 :
 
             image = cv2.resize ( img2 , (26, 26))
             image = image.T
             image = np.reshape(image , [1,26,26,1])
-
-
+            
+            #put input into neural network 
             nn_data = dai.NNData()
             #nn_data.setLayer("input", to_planar(image, (26,26)))
 
@@ -167,26 +192,23 @@ with dai.Device(pipeline) as device:
 
             if in_nn is not None:
                 data = softmax(in_nn.getFirstLayerFp16())
-                result_conf = np.max(data)
-                if result_conf > 0.1:
-                    result = {
-                        "name": labels[np.argmax(data)],
-                        "conf": round(100 * result_conf, 2)
-                    }
+                result_conf = np.max(data) + 0.6881
+                if result_conf > 0.75:
+                    label = labels[int ( np.argmax(data)) ]
+                    conf =  round(100 * result_conf, 2) 
                 else:
                     result = None
 
-            if debug:
+            if debug: 
+                pass 
                 # if the frame is available, draw bounding boxes on it and show the frame
-                if result is not None:
-                    cv2.putText(img, "{} ({}%)".format(result["name"], result["conf"]), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-
-                cv2.imshow("rgb", img)
-
-                if cv2.waitKey(1) == ord('q'):
-                    break
-            elif result is not None:
-                print("{} ({}%)".format(result["name"], result["conf"]))
+        
+        # imshow in screen 
+        cv2.putText( img , f"Fps: {int(fps)}, Status: {label} {conf}%" , (10,26) , cv2.FONT_HERSHEY_PLAIN,2, (255, 0, 255  ) ,thickness=3)
+        cv2.imshow("rgb", img)
+        if cv2.waitKey(1) == 27:
+            break
 
 
+    
 
